b5e83c7ced2adcec024ac5cd85f325ef3b43ae5a
Artur Konevnikov <artik292@gmail.com>
2017-10-06 09:49:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
 
+
 /*$button = $app->add(['Label', 'hello world']);
 $button->link('http://google.com/');
 
