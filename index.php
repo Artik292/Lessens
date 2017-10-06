9f220e8a6a2d8ae70dd497a1450ea3c32fd61285
Artur Konevnikov <artik292@gmail.com>
2017-10-06 12:00:53 +0300

    Adding files

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?PHP
 
 require 'vendor/autoload.php';
-//require 'conect.php';
-use \atk4\ui\Button;
-/* creating page */
-$app = new \atk4\ui\App('Registration');
+$app = new \atk4\ui\App('Space');
 $app->initLayout('Centered');
-/* button for people who are already registered */
-$button = new Button();
-$button->set('Log in');
-$button->set(['primary'=>true]);
-$button->set(['size small'=>true]);
-$button->link('login.php');
-$app->add($button);
 
-/*$img = 'https://www.nasa.gov/sites/default/files/thumbnails/image/ocs_iss_0.jpg';
+$img = 'https://www.nasa.gov/sites/default/files/thumbnails/image/ocs_iss_0.jpg';
 $button = $app->add(['Label', 'hello world','image'=>$img]);
 $button->set(['massive'=>true]);
 $button->link('http://google.com/');
@@ -22,7 +12,6 @@ $button->link('http://google.com/');
 $label = $app->add(['Label', 'hello world']);
 $label->link('https://google.com/');
 
-$img = 'https://www.nasa.gov/sites/default/files/thumbnails/image/ocs_iss_0.jpg';
 $app->add(['Label', 'МКС', 'image'=>$img,'Huge'=>true,'massive'=>true]);
 
 $text = $app->add(['Text', 'Некий текст']);
@@ -34,5 +23,4 @@ $text = $app->add('Text')
 $message = new \atk4\ui\Message('Message Title');
 $app->add($message);
 
-$img = 'https://www.nasa.gov/sites/default/files/thumbnails/image/ocs_iss_0.jpg';
-$icon = $app->add(['Image', $img, 'disabled']); */
+$icon = $app->add(['Image', $img, 'disabled']);
