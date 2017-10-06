8f9a4468219d84ad01441d791a510ea8e4e359e1
Artur Konevnikov <artik292@gmail.com>
2017-10-06 08:16:48 +0300

    Adding fiels

--- index.php
+++ index.php
@@ -1,52 +1,26 @@
 <?PHP
-//4_1
+
 require 'vendor/autoload.php';
+$app = new \atk4\ui\App('Registration');
+$app->initLayout('Centered');
 
-$app = new \atk4\ui\App('Icons and Links');
-$layout = $app->initLayout('Centered');
+$button = $app->add(['Label', 'hello world']);
+$button->link('http://google.com/');
 
-$button1 = $app->layout->add([
-	'View', 
-	'LINK TO PHP FILE'
-]);
-$button1->link(['4_2', 'id'=>123, 'name'=>'John']);  //если не указывать куда, то ссылается сам на себя
+$label = $app->add(['Label', 'hello world']);
+$label->link('http://google.com/');
 
-$button2 = $app->layout->add(
-	['Button', 'LINK TO 	TEXT FILE','iconRight'=>'book']
-);
-$button2->link('4_text.txt'); 
+$img = 'http://www.nasa.gov/sites/default/files/thumbnails/image/ocs_iss_0.jpg';
+$app->add(['Label', 'МКС', 'image'=>$img]);
 
-$button3 = $app->layout->add(['Button', 'LINK TO 	PDF FILE','iconRight'=>'student']);
-$button3->link('4_pdf.pdf'); 
+$text = $app->add(['Text', 'Некий текст']);
 
-$button4 = $app->layout->add(['Button', 'LINK TO 	FOTO','iconRight'=>'image']);
-$button4->link('4_image.jpg'); 
+$text = $app->add('Text')
+    ->addParagraph('Первый')
+    ->addParagraph('Второй');
 
-$button5 = $app->layout->add(['Button', 'LINK TO 	ICONS','iconRight'=>'idea']);
-$button5->link('https://semantic-ui.com/elements/icon.html'); 
+$message = new \atk4\ui\Message('Message Title');
+$app->add($message);
 
-
-
-
-
-/*$button = new Button('Link to program');
-$button->addClass('big');
-$button->icon = 'book';
-$button->link(['task','key'=>'ok']);
-//$button->link(['4_2.php', 'id'=123]);
-//$button->link(['4_2.php', 'id'=>123]);
-//$button->link('http://google.com/');
-//$button->link=(['http://www.agiletoolkit.org/intro/1']);
-//$button->js()->hide();
-$app->layout->add($button); */ 
-
-
-/*
-* use \atk4\ui\Button;
-* use \atk4\ui\Buttons;
-* use \atk4\ui\Header;
-* use \atk4\ui\Icon;
-* use \atk4\ui\Label;
-* use \atk4\ui\Template;
-* use \atk4\ui\View;
-*/
+$img = 'http://www.nasa.gov/sites/default/files/thumbnails/image/ocs_iss_0.jpg';
+$icon = $app->add(['Image', $img, 'disabled']);
