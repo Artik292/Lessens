5a0d32a7852df14857eb60c9126533e2c04f0c2c
Artur Konevnikov <artik292@gmail.com>
2017-10-12 21:30:32 +0300

    New file and 1 update

--- beatiful.php
+++ beatiful.php
@@ -1,22 +1,29 @@
 <?PHP
 
 require 'vendor/autoload.php';
-$app = new \atk4\ui\App('Links');
+$app = new \atk4\ui\App('Beautiful links');
 $layout = $app->initLayout('Centered');
+
+$view = $app->layout->add([
+	'View',
+	'LINK TO PHP FILE'
+]);
+$view->link(['4_2', 'id'=>123, 'name'=>'John']);  //если не указывать куда, то ссылается сам на себя
 
 $button1 = $app->layout->add(
 	['Button', 'LINK TO 	TEXT FILE','iconRight'=>'book']
 );
 $button1->link('4_text.txt');
+$button1->addClass('mini red');
 
 $button2 = $app->layout->add(['Button', 'LINK TO 	PDF FILE','iconRight'=>'student']);
 $button2->link('4_pdf.pdf');
+$button2->addClass('medium green');
 
 $button3 = $app->layout->add(['Button', 'LINK TO 	FOTO','iconRight'=>'image']);
 $button3->link('4_image.jpg');
+$button3->addClass('big blue');
 
 $button4 = $app->layout->add(['Button', 'LINK TO 	WEB-SITE','iconRight'=>'idea']);
 $button4->link('https://semantic-ui.com/elements/icon.html');
-
-$button5 = $app->layout->add(['Button','UPGRADE','icon'=>'space shuttle']);
-$button5->link(['beatiful']);
+$button4->addClass('massive pink');
