4dd7c119a144a91350e80d383623aea9cc369a90
Artur Konevnikov <artik292@gmail.com>
2017-10-12 21:44:20 +0300

    New file and 1 update

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ $layout = $app->initLayout('Centered');
 $button1 = $app->layout->add(
 	['Button', 'LINK TO 	TEXT FILE','iconRight'=>'book']
 );
-$button1->link('4_text.txt');
+//$button1->link('4_text.txt');
 
 $button2 = $app->layout->add(['Button', 'LINK TO 	PDF FILE','iconRight'=>'student']);
-$button2->link('4_pdf.pdf');
+//$button2->link('4_pdf.pdf');
 
 $button3 = $app->layout->add(['Button', 'LINK TO 	FOTO','iconRight'=>'image']);
-$button3->link('4_image.jpg');
+//$button3->link('4_image.jpg');
 
 $button4 = $app->layout->add(['Button', 'LINK TO 	WEB-SITE','iconRight'=>'idea']);
 $button4->link('https://semantic-ui.com/elements/icon.html');
